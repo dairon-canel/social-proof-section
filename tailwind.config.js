55c066ce863085a5992072c8b8b7e9d331c98ba6
Dairon Canel <98697567+RyuzakCoder@users.noreply.github.com>
2022-10-30 11:18:52 -0400

    feat: set challenge variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["public/*.{html,js}"],
+  content: ['public/*.{html,js}'],
   theme: {
     extend: {},
+    minWidth: {
+      375: '375px',
+    },
+    colors: {
+      VeryDarkMagenta: 'hsl(300, 43%, 22%)',
+      SoftPink: 'hsl(333, 80%, 67%)',
+      DarkGrayishMagenta: 'hsl(303, 10%, 53%)',
+      LightGrayishMagenta: 'hsl(300, 24%, 96%)',
+      White: 'hsl(0, 0%, 100%)',
+      noColor: 'hsla(0, 0%, 0%, 0)',
+    },
+    fontWeight: {
+      400: '400',
+      500: '500',
+      700: '700',
+    },
   },
   plugins: [],
-}
+};
