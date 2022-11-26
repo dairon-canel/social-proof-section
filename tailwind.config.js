2960fa0d7c680f304de50f40f06789900eccca0e
Dairon Canel <98697567+RyuzakCoder@users.noreply.github.com>
2022-11-26 18:17:18 -0500

    fix: config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/*.html'],
+  content: ['./public/index.html'],
+  purge: ['./public/index.html'],
   theme: {
     extend: {},
     minWidth: {
